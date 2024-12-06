4dbbff95a44428a88183b1bae5c1f58d4f98c5e2
ismail oruc <ioruc7176@gmail.com>
2024-12-06 17:07:48 +0300

    Added sub functions

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(void) {
+  int nota, notb, notc;
+  printf("Birinci notu girin:");
+  scanf("%d",&nota);
+  printf("ikinci notu girin:");
+  scanf("%d",&notb);
+  printf("ucuncu notu girin:");
+  scanf("%d",&notc);
+
+  if ((nota>=60 && notb>=60) || (nota>=60 && notc>=60) || (notb>=60 && notc>=60) )
+    printf("Gecti");
+  else
+    printf("Kaldi");
+  return 0;
 }
